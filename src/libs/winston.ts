72dc587e562e44ceea8eb8892d040754cfd95e77
gitmahin <nimahin25@gmail.com>
2026-09-15 00:00:45 +0600

    chore: update

--- src/libs/winston.ts
+++ src/libs/winston.ts
@@ -5,6 +5,7 @@ const options = {
   format: format.json(),
   transports: [
     new LokiTransport({
+      // Change the host with your ip to test localy
       host: "http://nebula-loki:3100",
       labels: { job: "nebula-server" },
       json: true,
